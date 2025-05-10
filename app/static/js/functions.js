31f37612d21a814eab5da7436298571ffd6e3ef5
LK <dev@remonetized.org>
2025-05-10 21:23:11 +0100

    collapsible sidebar and menu working

--- app/static/js/functions.js
+++ app/static/js/functions.js
@@ -19,3 +19,12 @@ function toggleHidden(elementID) {
         element.style.display = "none";
     }
 }
+
+function toggleDisplay(elementID) {
+  var element = document.getElementById(elementID);
+  if (element.className.indexOf("w3-show") == -1) {
+    element.className += " w3-show";
+  } else { 
+    element.className = element.className.replace(" w3-show", "");
+  }
+}
