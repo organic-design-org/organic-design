d98396974fb36eb0eed51c5a31d39a5b14506922
LK <dev@remonetized.org>
2025-05-13 00:36:42 +0100

    code output finished

--- app/functions.py
+++ app/functions.py
@@ -122,18 +122,75 @@ def rectJag(formValues):
     formValues['clipPath'] = 'polygon(' + str(coords) + ')'
     formValues['shapeOutside'] = 'polygon(' + str(coords) + ')'
 
+def show_code(formValues):
+    # HTML
+    htmlcode = ""
+    if formValues['divShadowCheck'] == "True" or formValues['divBorderCheck'] == "True":
+        htmlcode += '<div id="organicDivContainer">' 
+    htmlcode += "<div id='organicDiv'><div>"
+    if formValues['divShadowCheck'] == "True" or formValues['divBorderCheck'] == "True":
+        htmlcode += '</div>' 
+    # CSS
+    csscode = ""
+    if formValues['divShadowCheck'] == "True" or formValues['divBorderCheck'] == "True":
+        csscode += "#organicDivContainer {\n\tfilter: "
+        if formValues['divBorderCheck'] == "True":
+            csscode += f"\tdrop-shadow(1px 0px 0px {formValues['divBorderCol']})\n\t\t\t\tdrop-shadow(-1px 0px 0px {formValues['divBorderCol']})\n\t\t\t\tdrop-shadow(0px 1px 0px {formValues['divBorderCol']})\n\t\t\t\tdrop-shadow(0px -1px 0px {formValues['divBorderCol']})\n\t\t\t\tdrop-shadow(1px 1px 0px {formValues['divBorderCol']})\n\t\t\t\tdrop-shadow(-1px -1px 0px {formValues['divBorderCol']})\n\t\t\t\tdrop-shadow(-1px 1px 0px {formValues['divBorderCol']})\n\t\t\t\tdrop-shadow(1px -1px 0px {formValues['divBorderCol']})"
+        if formValues['divShadowCheck'] == "True" and formValues['divBorderCheck'] == "True":
+            csscode += "\n\t\t\t\t"
+        if formValues['divShadowCheck'] == "True":
+            csscode += f"drop-shadow(2px 8px 8px {formValues['divShadowCol']})"
+        csscode += ";\n}\n"
+    csscode += "#organicDiv {\n\t"
+    if formValues['divGradCheck'] == "True":
+        csscode += f"background-image: linear-gradient(to right, {formValues['divCol']}, {formValues['divGradCol']});"
+    else:
+        csscode += f"background-color: {formValues['divCol']};"
+    csscode += f"\n\theight: {formValues['divHeight']}px;\n\twidth: {formValues['divWidth']}px;"
+    if formValues['divStyle'] != 'testing':
+        csscode += f"\n\tclip-path: {formValues['clipPath']};"
+        csscode += f"\n\tshape-outside: {formValues['shapeOutside']};"
+    csscode += "\n}\n\n"
 
-def show_code(formValues):    
-    htmlcode = "<div id='organicDiv'><div>"
-    csscode = "#organicDiv {\n  background-color: " + str(formValues['divCol']) + ";\n  height: " + str(formValues['divHeight']) + "px;\n  width: " + str(formValues['divWidth']) + "px;"
-    sasscode = "$backgroundColour: " + str(formValues['divCol']) + ";\n$height: " + str(formValues['divHeight']) + "px;\n$width: " + str(formValues['divWidth']) + "px;"
-    jsoncode = '{\n"#organicDiv": {\n  "backgroundColour": "' + str(formValues['divCol']) + '",\n  "height": "' + str(formValues['divHeight']) + '",\n  "width": "' + str(formValues['divWidth']) + '"'
-    if formValues['divStyle'] == 'rectJag':
-        csscode += "\n  clip-path: polygon;"
-    if formValues['divStyle'] == 'rectCurve':
-        csscode += "\n  clip-path: path;"
-    csscode += "\n}"
-    jsoncode += "  \n}\n}"
+    # SASS
+    sasscode = ""
+    if formValues['divShadowCheck'] == "True" or formValues['divBorderCheck'] == "True":
+        sasscode += "\n$filter: "
+        if formValues['divBorderCheck'] == "True":
+            sasscode += f"drop-shadow(1px 0px 0px {formValues['divBorderCol']}) drop-shadow(-1px 0px 0px {formValues['divBorderCol']}) drop-shadow(0px 1px 0px {formValues['divBorderCol']}) drop-shadow(0px -1px 0px {formValues['divBorderCol']}) drop-shadow(1px 1px 0px {formValues['divBorderCol']}) drop-shadow(-1px -1px 0px {formValues['divBorderCol']}) drop-shadow(-1px 1px 0px {formValues['divBorderCol']}) drop-shadow(1px -1px 0px {formValues['divBorderCol']})"
+        if formValues['divShadowCheck'] == "True":
+            sasscode += f"drop-shadow(2px 8px 8px {formValues['divShadowCol']})"
+        sasscode += ";"
+    if formValues['divGradCheck'] == "True":
+        sasscode += f"$background-image: linear-gradient(to right, {formValues['divCol']}, {formValues['divGradCol']});"
+    else:
+        sasscode += f"$background-color: {formValues['divCol']};"
+    sasscode += f"\n$height: {formValues['divHeight']}px;\n$width: {formValues['divWidth']}px;"
+    if formValues['divStyle'] != 'testing':
+        sasscode += f"\n$clip-path: {formValues['clipPath']};\n$shape-outside: {formValues['shapeOutside']};"
+    sasscode += "\n\n"
+    
+    # JSON
+    jsoncode = "{"
+    if formValues['divShadowCheck'] == "True" or formValues['divBorderCheck'] == "True":
+        jsoncode += '\n"#organicDivContainer": {'
+        jsoncode += f'\n\t"filter": "'
+        if formValues['divBorderCheck'] == "True":
+            jsoncode += f"drop-shadow(1px 0px 0px {formValues['divBorderCol']}) drop-shadow(-1px 0px 0px {formValues['divBorderCol']}) drop-shadow(0px 1px 0px {formValues['divBorderCol']}) drop-shadow(0px -1px 0px {formValues['divBorderCol']}) drop-shadow(1px 1px 0px {formValues['divBorderCol']}) drop-shadow(-1px -1px 0px {formValues['divBorderCol']}) drop-shadow(-1px 1px 0px {formValues['divBorderCol']}) drop-shadow(1px -1px 0px {formValues['divBorderCol']}) "
+        if formValues['divShadowCheck'] == "True":
+            jsoncode += f"drop-shadow(2px 8px 8px {formValues['divShadowCol']})"
+        jsoncode += '"\n\t},'    
+    jsoncode += '\n"#organicDiv\": {'
+    if formValues['divGradCheck'] == "True":
+        jsoncode += f'\n\t"background-image": "linear-gradient(to right, {formValues['divCol']}, {formValues['divGradCol']})",'
+    else:
+        jsoncode += f'\n\t"background-color": "{formValues['divCol']}",'
+    jsoncode += f'\n\t"height": "{formValues['divHeight']}px",\n\t"width": "{formValues['divWidth']}px",'
+    if formValues['divStyle'] != 'testing':
+        jsoncode += f'\n\t"clip-path": "{formValues['clipPath']}",'
+        jsoncode += f'\n\t"shape-outside": "{formValues['shapeOutside']}"'
+    jsoncode += "\n\t}\n}\n\n"
+
     code = {
         "html": htmlcode,
         "css": csscode,
